6006c8e61822a5ae5e2e488f5c4bfc203dada9b1
Phionanamugga <phionanamugga15@gmail.com>
2019-01-25 15:23:12 +0300

    modifies  database file and coveragerc [ Delivers #163385275]

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from api.views import incident, user
-# from flask_jwt_extended import JWTManager
-
 
 app = Flask(__name__)
-# JWTManager(app)
 app.register_blueprint(incident)
 app.register_blueprint(user)
 
